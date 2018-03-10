3f972779424b7089b52713e6ef42b13e332a5ce3
tosshaan <tthapar@ucsd.edu>
2018-03-10 11:58:23 -0800

    firebase on tossh's computer

--- app/src/main/java/com/android/firebaseapp/MainActivity.java
+++ app/src/main/java/com/android/firebaseapp/MainActivity.java
@@ -6,10 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,6 +65,28 @@ public class MainActivity extends AppCompatActivity {
     {
         EditText etId = (EditText) findViewById(R.id.EditTextIdSearch);
         String idToSearch = etId.getText().toString();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myFirebaseRef = database.getReference();
+
+        Query queryRef = myFirebaseRef.orderByChild("studentId").equalTo(idToSearch);
+
+        queryRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot == null || snapshot.getValue() == null)
+                    Toast.makeText(MainActivity.this, "No record found", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Faile to read value
+                Log.w("TAG1", "Failed to read value.", error.toException());
+            }
+        });
+
     }
 
     public void submit(View button) {
@@ -68,6 +99,12 @@ public class MainActivity extends AppCompatActivity {
         Student student = new Student();
         student.setName(name);
         student.setStudentId(id);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myFirebaseRef = database.getReference();
+
+        myFirebaseRef.child(student.getStudentId()).setValue(student);
+
 
         Intent output = new Intent();
         setResult(RESULT_OK, output);
